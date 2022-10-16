455ff1c1c5f441f82fe2884303daf0b4b5822079
devcat22 <110813117+SulhaSion22@users.noreply.github.com>
2022-10-17 00:22:15 +0900

    Add files via upload

---
+++ p56.c
@@ -0,0 +1,13 @@
+/*#include <stdio.h>
+
+int main(void)
+{
+	int x = 9; //1001
+	printf("%d << 1 = %d\n", x, x << 1); // 10010 18
+	printf("%d >> 1 = %d\n", x, x >> 1); // 00100 4
+
+	return 0;
+
+
+}
+*/
